a578b1eeb3aaef406f1f89105dc4ee8ee70a5c31
Dinh Thanh Tai <taidt@vietmap.vn>
2023-11-14 20:41:12 +0700

    Init  asset management

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,31 @@ module.exports = {
     entry: './src/index.js',
     mode: 'production',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|tff|otf)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader']
+            }
+        ]
     }
 }
